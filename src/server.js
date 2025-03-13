1e81d8277435e6ce2794cf90dae3a40fa9dc938b
Andrea Nardinocchi <andrea.nardinocchi76@gmail.com>
2025-03-13 18:49:49 +0000

    Your commit message

--- src/server.js
+++ src/server.js
@@ -45,8 +45,9 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
+    // port: 3000,
+    // host: "localhost",
   });
 
   await server.register(Cookie); // registering the plugin
